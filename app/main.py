23b90c7300ac5c0553ca46b1e671d45ae1e83f72
Mi4BT <tb@mi4.fr>
2023-11-19 20:08:51 +0100

    feat: add file web request
    Ref: #15

--- app/main.py
+++ app/main.py
@@ -2,14 +2,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.orm import DeclarativeBase
+import yaml
+import hashlib
+import os
+
+def compute_hash(path):
+    sha256_hash = hashlib.sha256()
+    with open(path,"rb") as f:
+    # Read and update hash string value in blocks of 4K
+      for byte_block in iter(lambda: f.read(4096),b""):
+        sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
 
 class Base(DeclarativeBase):
   pass
 
+config = yaml.safe_load(open('./config.yaml').read())
+
 # create the app
 app = Flask(__name__)
 # configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = config["database"]["type"] + ":///" + config["database"]["uri"] #"sqlite:///project.db"
 
 
 db = SQLAlchemy(model_class=Base)
@@ -34,3 +47,14 @@ db.init_app(app)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World !</p>"
+
+@app.route("/download/<file_package>")
+def download(file_package):
+    module = Module.query.filter_by(package_name=file_package).first()
+    if module == None:
+        return 'module not found', 404
+    module_path = os.path.join(config["volume"]["path"], module.id_file + ".js")
+    module_hash = compute_hash(module_path)
+    if module_hash != module.hash:
+       return 'bad hash', 500
+    return open(module_path).read()
