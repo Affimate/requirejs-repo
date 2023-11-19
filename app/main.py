5f0023fc44556b6db758b756dc85f3ffcd4f1d2c
Mi4BT <tb@mi4.fr>
2023-11-19 19:21:31 +0100

    feat: Add new module table
    Ref: #15

--- app/main.py
+++ app/main.py
@@ -19,6 +19,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
 
+class Module(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    view_name = db.Column(db.String(128))
+    package_name = db.Column(db.String(128))
+    version = db.Column(db.String(128))
+    hash = db.Column(db.String(256))
+    id_file = db.Column(db.String(36))
+
 
 # initialize the app with the extension
 db.init_app(app)
